2ad23fd69ca108162fa7f3d2381c27bc60a3908e
Aakash Vashistha <aakashvashistha6@gmail.com>
2020-08-25 11:35:44 +0530

    2nd commit
    heroku deployment

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports={
-	database:'mongodb://localhost:27017/meanauth',
+	database:'mongodb+srv://aksh1002:Weakpass@meanauth.a5vux.mongodb.net/<meanauth>?retryWrites=true&w=majority',
 	secret:'9540219229'
 }
